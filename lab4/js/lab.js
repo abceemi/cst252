dd539d5e1c3917c397cedf1f014f6f2be8518ed1
Aimee Castellon <acastellonbustamante@csumb.edu>
2021-02-09 13:46:07 -0800

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+<pre class="language-javascript"><code>
+//Sign in to ilearn
+  //Accept username and password input
+    //Click on the 3 lines on the top left corner of the website
+        //Validate user click
+    //Select the desired course
+        //Validate course selection
+    //Under class name select grades
+        //Execute grade selection
+    //Scroll to the bottom and view current grade
+        //Display  information for user
+</code></pre>
