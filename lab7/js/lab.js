70da0065304ec3a1ab7e45424b92f5e3444ed069
Aimee Castellon <acastellonbustamante@csumb.edu>
2021-02-23 22:29:13 -0800

    LAB 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,27 +1,33 @@
-// lab6.js - Arrays and Objects
+// lab7.js - Functions
 // Author: Aimee Castellon
 // Created: February 16, 2021
 // License: Public Domain
 
 
-console.log("Arrays and Objects")
+//Create a new function in your file, call it what you like, but choose wisely.
+//In the function, declare a variable userName and use window.prompt() to get the user's name from the user. Here's some help.
+//Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays. Can you convert it back and forth?
+function userNameSort(){
+  // creates place to input name
+   var userName = window.prompt("What is your mothers maiden name?");
+   console.log ("userName =", userName);
+   //splitts the letters of the inputted name
+   var nameArray = userName.split('');
+   //sorts them in alphabetical order?
+   var nameArraySort = nameArray.sort();
+   //console output
+  console.log("nameArraySort =", nameArraySort);
+   //array to string
+   var nameSorted = nameArraySort.join('');
+   //console output
+   console.log("nameSorted =", nameSorted);
+   //Output
+   return nameSorted;
+ }
 
 
-// Declare variables
-myTransport = ["Mazda","BMW", "Mazda CX9", "My feet"];
 
-myMainRide = {
- make: "Mazda",
- model: "Mazda3",
- color: "grey",
- year: 2016,
- extras: "5sitter, effective",
- age: function() { return 2016 - this.year;}
+//Outside of the function, call the function and output the results with document.write() to neatly output the user's sorted name.
 
-
-
-// Use document.writeln() to neatly output each of your variables,
-document.writeln("Kinds of transportion I use: ", myTransport, "</br>");
-document.writeln("My Main Ride: <pre>",
-  JSON.stringify(myMainRide, null, '\t'), "</pre>");
-document.writeln("The age of my ", myMainRide.model, " is ", myMainRide.age(), " years.");
+document.write("Here is your Sorted Name:", userNameSort(), "</br>"," Now, what is your social security number?");
+ 
