a2ce307e9fdd3479d83122f908f51d1ddee8bf7c
Aimee Castellon <70286071+abceemi@users.noreply.github.com>
2021-02-28 17:42:06 -0800

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -16,13 +16,26 @@ console.log("Is 0+0 greater than or equal to 1?", addition(0,0));
 // Use map on your array to apply your function to all the numbers in your array.
 array = [10,2,3,5,6,0]
 console.log("My Array", array);
+//add array to document
+var newEl = document.createElement("p");
+newEl.innerHTML = " Original array:" + JSON.stringify(array);
+outputEl.appendChild(newEl);
 
 //array.map(yourFunctionName);
 var result = array.map(addition);
 console.log("Test of arrays:", result);
+//add array to document
+var newEl = document.createElement("p");
+newEl.innerHTML = " Original array:" + JSON.stringify(array);
+outputEl.appendChild(newEl);
+
 //Now use a new anonymous function with map to do some new operation on each of the numbers in your array.
-array.map(function(x,y){
-    var results = x-y;
-    return results;
+var result = array.map(function(x,y){
+    return results = x-y;
+
 })
 console.log("Addition",result);
+//add array to document
+var newEl = document.createElement("p");
+newEl.innerHTML = " Original array:" + JSON.stringify(array);
+outputEl.appendChild(newEl);
