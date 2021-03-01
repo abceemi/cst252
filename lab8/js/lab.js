6e2aadb18ec1c31fcbcaa324c9d65538a41d1ca5
Aimee Castellon <70286071+abceemi@users.noreply.github.com>
2021-02-28 16:51:09 -0800

    update to js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -11,14 +11,18 @@ function addition(x,y){
 }
 
 //Test your function with a few different numbers.
+console.log("Is 5+2 greater than or equal to 1?", addition(5,2));
+console.log("Is 0+0 greater than or equal to 1?", addition(0,0));
 // Use map on your array to apply your function to all the numbers in your array.
-array = [10,2,3,5,6,0];
+array = [10,2,3,5,6,0]
+console.log("My Array", array);
 
 //array.map(yourFunctionName);
-array.map(addition);
-
+var result = array.map(addition);
+console.log("Test of arrays:", result);
 //Now use a new anonymous function with map to do some new operation on each of the numbers in your array.
 array.map(function(x,y){
     var results = x-y;
     return results;
 })
+console.log("Addition",result);
