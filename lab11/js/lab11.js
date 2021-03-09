f251dac78bc5d003130b80cf69a834350bea4817
Aimee Castellon <acastellonbustamante@csumb.edu>
2021-03-09 11:24:09 -0800

    task 1

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -1,4 +1,15 @@
-// Lab 10: JavaScript Events and Forms
+// Lab 11: Conditionals
 // Author: Aimee Castellon
-// Created: March , 2021
+// Created: March 9, 2021
 // License: Public Domain
+
+//Create a function sortingHat()
+//that takes a string as an argument: function sortingHat(str)
+//counts the letters in str and assigns it to a variable len
+//uses modulus (% operator) to get the remainder with 4: mod = len % 4;
+//mod will now be a value between 0 and 3
+//create a conditional that will return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff depending on whether the value of mod is 0, 1, 2, or 3
+//Create an event listener attached to #button
+//that gets the value of #input and assigns it to a variable name
+//runs sortingHat(name) and stores the result in a variable house
+//appends a new styled paragraph to #output that says "The Sorting Hat has sorted you into " + house
