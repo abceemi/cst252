bca855acf981e6cc84738fe15072a0c664b5ed9b
your name here <your_email_here>
2021-03-21 23:42:07 -0700

    debugging

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -6,29 +6,32 @@
 //Create an array of numbers and assign it to a variable.
 //Create a named function that will take a single parameter, do a calculation on it (sqrt, sqr, addition, etc), and return the result.
 function addition(x,y){
-    var results = (x + y >= 1);
-    return results;
+    var result = (x + y >= 1);
+    return result;
 }
 
 //Test your function with a few different numbers.
 console.log("Is 5+2 greater than or equal to 1?", addition(5,2));
 console.log("Is 0+0 greater than or equal to 1?", addition(0,0));
+
 // Use map on your array to apply your function to all the numbers in your array.
-array = [10,2,3,5,6,0]
-console.log("My Array", array);
+array = [10,2,3,5,6,0];
+console.log("Original array", array);
+
 //add array to document
-var newEl = document.createElement("p");
-newEl.innerHTML = " Original array:" + JSON.stringify(array);
-outputEl.appendChild(newEl);
+//var newEl = document.createElement("p");
+//newEl.innerHTML = " Original array:" + JSON.stringify(array);
+//outputEl.appendChild(newEl);
 
 //array.map(yourFunctionName);
 var result = array.map(addition);
-console.log("Test of arrays:", result);
+console.log("Test arrays:", result);
 
 //Now use a new anonymous function with map to do some new operation on each of the numbers in your array.
 var result = array.map(function(x,y){
-    return results = x-y;
+    return result = x-y;
 
 })
 console.log("addition",result);
+//new array results
 document.write("addition",result);
