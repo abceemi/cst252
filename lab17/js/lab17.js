68d3227de00029f1e6e809013aea0fa112031b85
your name here <your_email_here>
2021-04-09 01:04:37 -0700

    lab17

---
+++ lab17/js/lab17.js
@@ -0,0 +1,22 @@
+// Lab 17: Advanced jQuery
+// Author: Aimee Castellon
+// Created: April 10, 2021
+// License: Public Domain
+
+//Using jQuery, do the following:
+//Add a click event to each button
+//In the event code, toggle a new class within the div
+//Go into your css and style each of the classes
+//Test each button
+
+$("#button").click(function(){
+  $("#button").toggleClass("now")
+  var input = $("#inputText").val();
+  $("#output").html(input);
+});
+
+$("#button2").click(function(){
+  $("#button2").toggleClass("now2")
+  var input = $("#inputText").val();
+  $("#output2").html(input2);
+});
